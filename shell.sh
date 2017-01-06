95e0b1a9523002e4d2c88d733e95cd3c459fcb06
Alex Balakersky <alex@balakersky.com>
2017-01-06 10:11:07 -0500

    Updated encryption script

--- shell.sh
+++ shell.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-while [[ $# -gt 1 ]]
-do
-key="$1"
-
-case $key in
-    -e|--email)
-    EMAILPASS="$2"
-    shift # past argument
-    ;;
-    -l|--login)
-    LOGINPASS="$2"
-    shift # past argument
-    ;;
-    *)
-            # unknown option
-    ;;
-esac
-shift # past argument or value
-done
-
-if [ -z "$EMAILPASS" -a "$EMAILPASS" != " " ]; 
-then
+if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+        echo "
+        usage : encryption_utils.sh [-e|--email] [-l|--login] <password>
+        Option provided by script:
+          -e,  --email    Specifying the password given is email password
+          -l,  --login    Specifying the password given is login password
+          <password>      Password"
+else
+    while [[ $# -gt 1 ]]
+    do
+    key="$1"
+    case $key in
+           -e|--email)
+           EMAILPASS="$2"
+           shift # past argument
+           ;;
+           -l|--login)
+           LOGINPASS="$2"
+           shift # past argument
+           ;;
+           *)
+           # unknown option
+           ;;
+    esac
+        shift # past argument or value
+    done
+    if [ -z "$EMAILPASS" -a "$EMAILPASS" != " " ];
+    then
         echo "You have not entered email password"
-else
-	echo $(/opt/dnow/embedded/jre/bin/java -cp '/opt/dnow/webapps/DeployNow/WEB-INF/lib/*' com.opex.dnow.core.util.EncryptionUtils --encrypt $EMAILPASS)
-fi
-
-if [ -z "$LOGINPASS" -a "$LOGINPASS" != " " ]; then
+    else
+        echo $(<%= install_dir %>/embedded/jre/bin/java -cp '<%= install_dir %>/webapps/DeployNow/WEB-INF/lib/*' com.opex.dnow.core.util.EncryptionUtils --encrypt $EMAILPAS)
+    fi
+    if [ -z "$LOGINPASS" -a "$LOGINPASS" != " " ]; then
         echo "Login Password not entered!"
-else
-	echo $(/opt/dnow/embedded/jre/bin/java -cp '/opt/dnow/webapps/DeployNow/WEB-INF/lib/*' com.opex.dnow.core.util.EncryptionUtils --md5 $LOGINPASS)
+    else
+        echo $(<%= install_dir %>/embedded/jre/bin/java -cp '<%= install_dir %>/webapps/DeployNow/WEB-INF/lib/*' com.opex.dnow.core.util.EncryptionUtils --md5 $LOGINPAS)
+    fi
 fi
